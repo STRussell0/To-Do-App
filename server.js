a45200522eec2e26ccc654ab07de63e1dbc421f7
Stephen Russell <srussell@alphaworks.tech>
2021-08-27 05:57:51 -0700

    Update: Set up for heroku

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ let app = express();
 
 let db
 
+let port = process.env.PORT
+if (port == null OR port == "") {
+  port = 3000
+}
+
 app.use(express.static('public'))
 
 let connectionString = 'mongodb+srv://Jafar:pass@cluster0.ojde6.mongodb.net/TodoApp?retryWrites=true&w=majority'
 
 mongodb.MongoClient.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   db = client.db()
-  app.listen(3000)
+  app.listen(port)
 })
 
 app.use(express.json())
